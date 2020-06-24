7cc1c181c555c09fe6e4b039fe18ce1b00759709
Jasvinder Singh <jasvinder.singh@appdynamics.com>
2020-06-24 17:18:51 +0530

    New test code addition

--- Basic_programs/demo8.py
+++ Basic_programs/demo8.py
@@ -1,5 +1,10 @@
 ###
-#program to print all the prime number in given interval
+# program to print all the prime number in given interval
+# In most of the programming languages (C/C++, Java, etc), 
+# the use of else statement has been restricted with the if conditional statements. 
+# But Python also allows us to use the else condition with for loops. 
+# The else block just after for/while is executed only 
+# when the loop is NOT terminated by a break statement.
 ###
 
 #def prime_num_finder():
@@ -7,19 +12,21 @@
 #       if num % 2 != 0:
 #            print(num, "is a prime number")
 
-        
-#if __name__ == "__main__":
-#    prime_num_finder()
+def prime_num_finder(low_num , high_num):
+    for num in range(low_num , high_num + 1):
+        if num > 0:
+            for i in range(2 , num):
+                if num % i == 0:
+                    break
+            else:
+                print(num)    
+       
+if __name__ == "__main__":
+    print("Enter the lowest number:")
+    low_num = int(input())
+    print("Enter the highest number:")
+    high_num = int(input())
+    prime_num_finder(low_num , high_num)
 
-
-low_num = int(input())
-high_num = int(input())
-for num in range(low_num, high_num + 1):
-    if num > 0:
-        for i in range(2 , num):
-            if num % i == 0:
-                break
-        else:
-            print(num)
 
             
