ef6d9e6f0f39cb617c120a539c86e4e13f83c50a
Jasvinder Singh <jasvinder.singh@appdynamics.com>
2020-07-13 18:51:12 +0530

    triple quotes

---
+++ Basic_programs/triple_quotes.py
@@ -0,0 +1,13 @@
+###
+# Breaking the long lines into readable form is done using triple single 
+#or double quotes
+##
+def triple_quoter():
+    para_str = """ This is a really long comment that can make  
+the code look ugly and uncomfortable to read on 
+a small screen so it needs to be broken into  
+multi-line comments using double triple-quotes"""
+    print(para_str)
+
+if __name__ == "__main__":
+    triple_quoter()
