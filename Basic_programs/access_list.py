9ce0f13f750a96426528c2eeef56999d92bc15a9
Jasvinder Singh <jasvinder.singh@appdynamics.com>
2020-07-13 18:58:25 +0530

    Acessing list elements

---
+++ Basic_programs/access_list.py
@@ -0,0 +1,15 @@
+###
+# Accessing list elements
+###
+
+def list_access(fruits, numbers):
+    print(fruits[1:3])
+    print(fruits[3])
+    print(numbers[1:5])
+
+
+
+if __name__ == "__main__":
+    fruits = ['Apple', 'Bannana', 'Jackfruit', 'Papaya', 'Grapes']
+    numbers = [1,2,3,4,5,6,7,8,9]
+    list_access(fruits, numbers)
