e5c5edd0ddfa4d25678ee6bfa34b4f015726993e
Jasvinder Singh <jasvinder.singh@appdynamics.com>
2020-07-28 02:01:23 +0530

    optimised_bubble_sort

---
+++ array_programs/optimised_bubble_sort.py
@@ -0,0 +1,22 @@
+###
+# Bubble sort program
+###
+
+
+def bubble_sort(arr , n):
+    swapped = True
+    while swapped:
+        swapped = False 
+        for j in range(0,n-1):
+            if arr[j] > arr[j+1]:
+                swapped = True
+                temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+                
+    print(arr)            
+
+if __name__ == "__main__":
+    arr = [3,5,6,2,8,2,1]
+    n = len(arr)
+    bubble_sort(arr , n)
