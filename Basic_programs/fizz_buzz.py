1f2ad76fe2e5501f26899ba9d287ad7445c3f4da
Jasvinder Singh <jasvinder.singh@appdynamics.com>
2020-07-28 17:59:30 +0530

    Fizz buzz

--- Basic_programs/fizz_buzz.py
+++ Basic_programs/fizz_buzz.py
@@ -2,16 +2,27 @@
 # Program to print number form 1 to 100
 ###
 
-def fizz_buzz():
-    for i in range(1,100):
+def fizz_buzz(n):
+    m = []
+    for i in range(1,n+1):
         if i % 3 == 0 and i % 7 == 0:
-            print("Fizz buzz")
+            m.append("Fizzbuzz")
         elif i % 3 == 0:
-            print("Fizz")    
+            m.append("Fizz")   
         elif i % 7 == 0:
-            print("buzz")
+            m.append("Buzz")   
         else:
-            print(i)            
+            m.append(i)    
+    return m        
+
+def test_case(k , expected_output):
+    if k == expected_output:
+        print("Test case pass")
+    else:
+        print("Test case failed")    
 
 if __name__ == "__main__":
-    fizz_buzz()
+    k = fizz_buzz(5)
+    print(k)
+    expected_output = [1, 2, 'Fizz', 4, 5]
+    test_case(k , expected_output)
